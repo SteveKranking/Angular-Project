885cd5f35d8a3fb1edd83a283224307b969ddd7b
Stephen Kranking <stephenkranking@yahoo.com>
2018-02-20 16:29:02 -0500

    recipes model testing, some bootstrap

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', "Test Desc", "https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fbourbon-pecan-tassies.jpg%3Fitok%3D1mUFJSV-&w=800&q=85")
+  ];
 
   constructor() { }
 
